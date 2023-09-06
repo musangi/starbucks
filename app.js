522611d3e23efab1b60d19cd322f8ffdc609cc5d
musangi <faithmusangi50@gmail.com>
2023-09-06 16:29:24 +0300

    add menu icon and styling

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ function changeCircleColor(color){
     const circle = document.querySelector('.circle');
     circle.style.background = color;
 }
+
+function toggleMenu() {
+    var toggleMenu = document.querySelector('.toggle');
+    var nav = document.querySelector('.nav');
+    toggleMenu.classList.toggle('active');
+    nav.classList.toggle('active');
+}
